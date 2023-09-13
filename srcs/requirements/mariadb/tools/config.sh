2ddea9be3b8aa3a6cf4e514ac2f2655b3910b689
omanar <omanar@student.1337.ma>
2023-09-13 04:17:58 +0100

    make directory structure as mentioned in the subject and edited on mariadb

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -2,8 +2,13 @@
 
 service mariadb start
 
+sleep 5
+
+mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mariadb -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -u root -e "USE ${MYSQL_DATABASE};"	
+
+service mariadb stop
