c80872c2bebd6e5674cee0b1ef554daf231afc10
omanar <omanar@student.1337.ma>
2023-09-13 10:53:22 +0100

    All configs done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,13 @@
+mkdir -p /run/php/
+
 mkdir -p /var/www/html/wordpress
 
 cd /var/www/html/wordpress
 
 wp core download --allow-root
 
-wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
 
-wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --a
+wp core install --url=${DOMAIN_NAME} --title=${WORDPRESS_DB_TITLE} --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL} --allow-root
 
-exec $@
+php-fpm7.4 -R -F
