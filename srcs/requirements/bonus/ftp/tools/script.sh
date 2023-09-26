19a23df679cf2c0c98b2b2840057588a19fe41d6
omanar <omanar@student.1337.ma>
2023-09-26 12:43:33 -0500

    finally ftp fixeed

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -8,6 +8,7 @@ mkdir -p /var/run/vsftpd/empty
 cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
 
 adduser --disabled-password --gecos "" $FTP_USER
+usermod -aG sudo $FTP_USER
 echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
 chown -R $FTP_USER:$FTP_USER /var/www/html
 echo $FTP_USER >> /etc/vsftpd.userlist
